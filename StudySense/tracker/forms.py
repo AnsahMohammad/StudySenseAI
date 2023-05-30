b5184694597f24ef2cdff5f141d83b0748f016c2
AnsahMohammad <mohdansah10@gmail.com>
2023-05-30 15:38:10 +0530

    category selection

--- StudySense/tracker/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Book
-from django import forms
-
-class PDFUploadForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ["name", "category", "file"]
--- StudySense/tracker/views.py
+++ StudySense/tracker/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render, redirect
-from django.conf import settings
-from .forms import PDFUploadForm
-from .models import Book
+from django.shortcuts import render, redirect, Http404
+from .models import Book, Category
 
 def upload_pdf(request):
     if request.method == 'POST':
-        form = PDFUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            file = form.save()
-            return redirect('display', pk=file.pk)
-    else:
-        form = PDFUploadForm()
-    return render(request, 'upload_pdf.html', {"form": form})
+        name = request.POST.get('name')
+        category_id = request.POST.get('cat') 
+        file = request.FILES.get('myfile')
+
+        category_ = Category.objects.get(id=category_id)
+        new_book = Book.objects.create(
+            name=name,
+            category=category_,
+            file=file
+        )
+        return redirect('display', pk=new_book.pk)
+    categories = Category.objects.all()
+    return render(request, 'upload_pdf.html', {"categories": categories})
 
 def view_book(request, pk):
     pdf = Book.objects.get(pk=pk)
