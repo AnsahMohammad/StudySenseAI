a21ddee4faa417ab8b817d478f7c8e8756ca57d6
AnsahMohammad <mohdansah10@gmail.com>
2023-06-01 22:34:24 +0530

    Added time tracking

--- StudySense/tracker/models.py
+++ StudySense/tracker/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50, null=False)
-
+    total_time = models.FloatField(default=0)
 
 class Book(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
@@ -13,3 +13,4 @@ class Book(models.Model):
     )
     file = models.FileField()
     created_at = models.DateTimeField(auto_now_add=True)
+    total_time = models.FloatField(default=0)
--- StudySense/tracker/urls.py
+++ StudySense/tracker/urls.py
@@ -4,4 +4,5 @@ from tracker import views
 urlpatterns = [
     path("", views.upload_pdf, name="home"),
     path("display/<int:pk>/", views.view_book, name="display"),
+    path("record/", views.record_time, name="record_time"),
 ]
