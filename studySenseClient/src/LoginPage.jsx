5025d8674fbf406618cb0de7831e2c1bd8ae1e2d
AnsahMohammad <mohdansah10@gmail.com>
2023-06-19 21:30:25 +0530

    Added login page template

---
+++ studySenseClient/src/LoginPage.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
+}
+from 'mdb-react-ui-kit';
+import './Login.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <MDBContainer className="my-5">
+
+      <MDBCard>
+        <MDBRow className='g-0'>
+
+          <MDBCol md='6'>
+            <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp' alt="login form" className='rounded-start w-100'/>
+          </MDBCol>
+
+          <MDBCol md='6'>
+            <MDBCardBody className='d-flex flex-column'>
+
+              <div className='d-flex flex-row mt-2'>
+                <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }}/>
+                <span className="h1 fw-bold mb-0">/StudySenseAI</span>
+              </div>
+
+              <h5 className="fw-normal my-4 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
+
+                <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
+                <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
+
+              <MDBBtn className="mb-4 px-5" color='dark' size='lg'>Login</MDBBtn>
+              <a className="small text-muted" href="#!">Forgot password?</a>
+              <p className="mb-5 pb-lg-2" style={{color: '#393f81'}}>Don't have an account? <a href="#!" style={{color: '#393f81'}}>Register here</a></p>
+
+              <div className='d-flex flex-row justify-content-start'>
+                <a href="#!" className="small text-muted me-1">Terms of use.</a>
+                <a href="#!" className="small text-muted">Privacy policy</a>
+              </div>
+
+            </MDBCardBody>
+          </MDBCol>
+
+        </MDBRow>
+      </MDBCard>
+
+    </MDBContainer>
+  );
+}
+
+export default App;
--- studySenseClient/src/main.jsx
+++ studySenseClient/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Register from "./Register";
+import Login from './LoginPage';
 
 ReactDOM.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Router>,
   document.getElementById("root")
