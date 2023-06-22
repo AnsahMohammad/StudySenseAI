ba703fd2653c7533a3a45c437bd028dcf92955bb
AnsahMohammad <mohdansah10@gmail.com>
2023-06-22 07:51:55 +0530

    updated dev.sh

--- dev.sh
+++ dev.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Define color variables
+GREEN='\033[0;32m'  # Green color
+NC='\033[0m'       # No color
+
+echo -e "${GREEN}Clearing the ports 8000 and 5173${NC}"
+sudo lsof -ti :8000 | xargs --no-run-if-empty kill
+sudo lsof -ti :5173 | xargs --no-run-if-empty kill
+
+echo -e "${GREEN}Starting Django${NC}"
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
@@ -9,7 +18,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver &
 
+echo -e "${GREEN}Starting React${NC}"
 cd ../studySenseClient
-npm install react-router-dom
 npm install
 npm run dev -- --port 5173
