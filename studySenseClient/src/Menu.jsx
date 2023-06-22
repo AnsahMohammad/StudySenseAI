a93b124b8ac29b8f545f443ed1d90cc54c5694ee
AnsahMohammad <mohdansah10@gmail.com>
2023-06-22 08:33:11 +0530

    updated UI to notion based

--- studySenseClient/src/Menu.jsx
+++ studySenseClient/src/Menu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+import { MDBContainer, MDBRow, MDBCol, MDBAccordion, MDBAccordionItem } from "mdb-react-ui-kit";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Styling/Menu.css";
 
@@ -9,6 +9,24 @@ function App() {
       <MDBRow className="custom-row">
         <MDBCol className="custom-col secondary">
           <h2>DOCK</h2>
+          <div className="secondary-accordion">
+            <MDBAccordion initialActive={1} className="w-100">
+              <MDBAccordionItem collapseId={1} headerTitle="MATH">
+                <ul>
+                  <li>Algebra</li>
+                  <li>Geometry</li>
+                  <li>Calculus</li>
+                </ul>
+              </MDBAccordionItem>
+              <MDBAccordionItem collapseId={2} headerTitle="Physics">
+                <ul>
+                  <li>Quantum</li>
+                  <li>Newton</li>
+                  <li>Hamiltonian</li>
+                </ul>
+              </MDBAccordionItem>
+            </MDBAccordion>
+          </div>
         </MDBCol>
         <MDBCol className="custom-col primary">
           <h1>MAIN</h1>
