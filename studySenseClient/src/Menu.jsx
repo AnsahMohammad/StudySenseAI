7df3f1ca846c17be3b4116f3adde22cc216be975
AnsahMohammad <mohdansah10@gmail.com>
2023-06-23 07:57:04 +0530

    updated UI

--- studySenseClient/src/Menu.jsx
+++ studySenseClient/src/Menu.jsx
@@ -5,40 +5,45 @@ import Cookies from 'universal-cookie';
 import "./Styling/Menu.css";
 
 function App() {
-  const cookies = new Cookies();
-  const user = cookies.get('user');
+	const cookies = new Cookies();
+	const user = cookies.get('user');
 
-  return (
-    <MDBContainer fluid className="p-0 m-0 h-100">
-      <MDBRow className="main-page">
-        <MDBCol className="main-border secondary">
-          <h2>{user.username}</h2>
-          <p>Email: {user.email}</p>
-          <div className="secondary-accordion">
-            <MDBAccordion initialActive={1} className="w-100">
-              <MDBAccordionItem collapseId={1} headerTitle="MATH">
-                <ul>
-                  <li>Algebra</li>
-                  <li>Geometry</li>
-                  <li>Calculus</li>
-                </ul>
-              </MDBAccordionItem>
-              <MDBAccordionItem collapseId={2} headerTitle="Physics">
-                <ul>
-                  <li>Quantum</li>
-                  <li>Newton</li>
-                  <li>Hamiltonian</li>
-                </ul>
-              </MDBAccordionItem>
-            </MDBAccordion>
-          </div>
-        </MDBCol>
-        <MDBCol className="main-border primary">
-          <h1>MAIN</h1>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
+	return (
+		<MDBContainer fluid className="p-0 m-0 h-100">
+			<MDBRow className="main-page">
+				<MDBCol className="main-border secondary p-0">
+					<MDBContainer className="user-tab">
+						<p>{user.username} - {user.email}</p>
+					</MDBContainer>
+					<div className="secondary-accordion">
+						<MDBAccordion initialActive={1} className="w-100">
+							<MDBAccordionItem collapseId={1} className="drop-item" headerTitle="MATH">
+								<MDBContainer fluid className="m-0 file-holder">
+									<ul>
+										<li>Algebra</li>
+										<li>Geometry</li>
+										<li>Calculus</li>
+									</ul>
+								</MDBContainer>
+							</MDBAccordionItem>
+							<MDBAccordionItem collapseId={2} className="drop-item" headerTitle="Physics">
+								<MDBContainer fluid className="m-0 file-holder">
+									<ul>
+										<li>Algebra</li>
+										<li>Geometry</li>
+										<li>Calculus</li>
+									</ul>
+								</MDBContainer>
+							</MDBAccordionItem>
+						</MDBAccordion>
+					</div>
+				</MDBCol>
+				<MDBCol className="main-border primary">
+					<h1>Welcome {user.username}</h1>
+				</MDBCol>
+			</MDBRow>
+		</MDBContainer>
+	);
 }
 
 export default App;
