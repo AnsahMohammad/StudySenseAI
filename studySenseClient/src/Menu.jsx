093471d44b1a058d7fdd98d9b5f94c37a9204ab9
AnsahMohammad <mohdansah10@gmail.com>
2023-06-23 08:01:47 +0530

    Added clickable files

--- studySenseClient/src/Menu.jsx
+++ studySenseClient/src/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBAccordion, MDBAccordionItem } from "mdb-react-ui-kit";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Cookies from 'universal-cookie';
@@ -7,6 +7,11 @@ import "./Styling/Menu.css";
 function App() {
 	const cookies = new Cookies();
 	const user = cookies.get('user');
+	const [selectedItem, setSelectedItem] = useState(null);
+
+	const handleItemClick = (itemName) => {
+		setSelectedItem(itemName);
+	};
 
 	return (
 		<MDBContainer fluid className="p-0 m-0 h-100">
@@ -19,20 +24,28 @@ function App() {
 						<MDBAccordion initialActive={1} className="w-100">
 							<MDBAccordionItem collapseId={1} className="drop-item" headerTitle="MATH">
 								<MDBContainer fluid className="m-0 file-holder">
-									<ul>
-										<li>Algebra</li>
-										<li>Geometry</li>
-										<li>Calculus</li>
-									</ul>
+									<div className={`selectable-item ${selectedItem === 'Algebra' ? 'selected' : ''}`} onClick={() => handleItemClick('Algebra')}>
+										Algebra
+									</div>
+									<div className={`selectable-item ${selectedItem === 'Geometry' ? 'selected' : ''}`} onClick={() => handleItemClick('Geometry')}>
+										Geometry
+									</div>
+									<div className={`selectable-item ${selectedItem === 'Calculus' ? 'selected' : ''}`} onClick={() => handleItemClick('Calculus')}>
+										Calculus
+									</div>
 								</MDBContainer>
 							</MDBAccordionItem>
 							<MDBAccordionItem collapseId={2} className="drop-item" headerTitle="Physics">
 								<MDBContainer fluid className="m-0 file-holder">
-									<ul>
-										<li>Algebra</li>
-										<li>Geometry</li>
-										<li>Calculus</li>
-									</ul>
+									<div className={`selectable-item ${selectedItem === 'Physics Algebra' ? 'selected' : ''}`} onClick={() => handleItemClick('Physics Algebra')}>
+										Physics Algebra
+									</div>
+									<div className={`selectable-item ${selectedItem === 'Physics Geometry' ? 'selected' : ''}`} onClick={() => handleItemClick('Physics Geometry')}>
+										Physics Geometry
+									</div>
+									<div className={`selectable-item ${selectedItem === 'Physics Calculus' ? 'selected' : ''}`} onClick={() => handleItemClick('Physics Calculus')}>
+										Physics Calculus
+									</div>
 								</MDBContainer>
 							</MDBAccordionItem>
 						</MDBAccordion>
@@ -40,6 +53,7 @@ function App() {
 				</MDBCol>
 				<MDBCol className="main-border primary">
 					<h1>Welcome {user.username}</h1>
+					{selectedItem && <h2>{selectedItem}</h2>}
 				</MDBCol>
 			</MDBRow>
 		</MDBContainer>
