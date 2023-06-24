642f5ef537f5286b76e9209df4858c8667bd5943
AnsahMohammad <mohdansah10@gmail.com>
2023-06-24 20:27:48 +0530

    fixed migrations

--- server.sh
+++ server.sh
@@ -4,7 +4,8 @@ python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
 
-cd StudySense
+cd study_sense
+export DJANGO_KEY="test"
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
--- study_sense/study_sense/settings.py
+++ study_sense/study_sense/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = "study_sense.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db2.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
