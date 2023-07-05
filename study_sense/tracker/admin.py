340d982008c97b164f50e4e5da3d2b3e255d7347
AnsahMohammad <mohdansah10@gmail.com>
2023-07-05 08:32:59 +0530

    registered on admin

--- study_sense/tracker/admin.py
+++ study_sense/tracker/admin.py
@@ -2,9 +2,10 @@
 Admin Module for tracker app
 """
 from django.contrib import admin
-from .models import Book, Category
+from .models import Book, Category, TimeTracking
 
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Category)
+admin.site.register(TimeTracking)
