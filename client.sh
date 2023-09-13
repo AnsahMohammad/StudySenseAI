0c69266f4a8883e5063ce4b267e6a8d100bbc00f
AnsahMohammad <mohdansah10@gmail.com>
2023-09-13 20:18:29 +0530

    Django deploy ready

--- client.sh
+++ client.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd ../studySenseClient
+cd ./studySenseClient
+echo -e "${GREEN}Starting React${NC}"
+# npm install mdb-react-ui-kit
+# npm install react-cookie
+# npm install bootstrap
+# npm install react-pdf @react-pdf/renderer
+# npm install semantic-ui-react semantic-ui-css
+# npm install react-apexcharts apexcharts
 npm install
 npm run dev -- --port 5173
