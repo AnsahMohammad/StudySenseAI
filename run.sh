c54f317a6c8547d453530f2388c5ad1994643fef
AnsahMohammad <mohdansah10@gmail.com>
2023-09-14 11:45:39 +0530

    added migrations

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ pip3 install -r requirements.txt
 
 cd study_sense
 export DJANGO_KEY=DJANGO_KEY
+python3 manage.py makemigrations
+python3 manage.py migrate
 gunicorn -c gunicorn_config.py study_sense.wsgi:application
